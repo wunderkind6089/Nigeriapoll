8992f5dce948849075fe1389cb238f5724e698e8
Abdulquadri <olayanju.olaide@olivetsolutions>
2019-05-18 09:47:22 +0100

    Nigeriapoll First Commit

---
+++ precache-manifest.cdd7114a85a7c6762c311fd31e3a8f8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc488ffc011601e81a7713c142485673",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fe0bf36b439b433ba92",
+    "url": "/static/css/2.74d8aad0.chunk.css"
+  },
+  {
+    "revision": "66db660745cbde5aa0bf",
+    "url": "/static/css/main.21dc5609.chunk.css"
+  },
+  {
+    "revision": "8fe0bf36b439b433ba92",
+    "url": "/static/js/2.1c2432c8.chunk.js"
+  },
+  {
+    "revision": "66db660745cbde5aa0bf",
+    "url": "/static/js/main.729f6827.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
